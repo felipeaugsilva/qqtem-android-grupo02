adf31e8bcb2e7bd9c244335d53e31004889dbffd
felipe.aug.silva <felipe.aug.silva@8477728c-a87f-69e8-18fb-67d25cc80549>
2011-11-03 21:18:37 +0000

    Criação do projeto com algumas funções já implementadas: - Abas (falta mudar os ícones); - Aba de mapa; - Menu (não funcional ainda).

---
+++ trunk/QqTem/src/unicamp/mc750/qqtem/Map.java
@@ -0,0 +1,34 @@
+package unicamp.mc750.qqtem;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class Map extends MapActivity {
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+        
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        
+        mapView.setBuiltInZoomControls(true);
+    }
+    
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, R.integer.MENU_ADD_ID, 0, R.string.menu_add);
+        menu.add(0, R.integer.MENU_LOGIN_ID, 0, R.string.menu_login);
+        return true;
+    }
+}
