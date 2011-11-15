a941ce696d3fcbb97f13e5149aeff362a94e6e6c
felipe.aug.silva <felipe.aug.silva@8477728c-a87f-69e8-18fb-67d25cc80549>
2011-11-15 20:46:12 +0000

    Tela de login.

--- QqTem/src/unicamp/mc750/qqtem/List.java
+++ QqTem/src/unicamp/mc750/qqtem/List.java
@@ -1,8 +1,10 @@
 package unicamp.mc750.qqtem;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -30,5 +32,37 @@ public class List extends Activity {
         menu.add(0, R.integer.MENU_ADD_ID, 0, R.string.menu_add);
         menu.add(0, R.integer.MENU_LOGIN_ID, 0, R.string.menu_login);
         return true;
+    }
+	
+	@Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch(item.getItemId()) {
+            case R.integer.MENU_LOGIN_ID:
+            	showDialog(R.integer.DIALOG_LOGIN_ID);
+            	return true;
+            case R.integer.MENU_ADD_ID:
+            	showDialog(R.integer.DIALOG_ADD_ID);
+            	return true;
+        }
+
+        return super.onMenuItemSelected(featureId, item);
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+        
+        case R.integer.DIALOG_LOGIN_ID:
+        	Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.login);
+            dialog.setTitle("Login");
+            return dialog;
+            
+        case R.integer.DIALOG_ADD_ID:
+        	return null;
+        
+        default:
+        	return null;
+        }
     }
 }
