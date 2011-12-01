7e780e12193919b08aaaee8393ccb922a579478c
felipe.aug.silva <felipe.aug.silva@8477728c-a87f-69e8-18fb-67d25cc80549>
2011-12-01 18:45:32 +0000

    Exibição de dialogos ao enviar comentario ou reportar abuso.

--- trunk/QqTem/src/unicamp/mc750/qqtem/ReportAbuse.java
+++ trunk/QqTem/src/unicamp/mc750/qqtem/ReportAbuse.java
@@ -1,7 +1,9 @@
 package unicamp.mc750.qqtem;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -9,6 +11,9 @@ import android.widget.Button;
 public class ReportAbuse extends Dialog implements OnClickListener {
 
 	Button okButton, cancellButton;
+	Context mContext;
+	ReportAbuse report = this;
+	AlertDialog.Builder builder;
 	
     public ReportAbuse(Context context) {
 		super(context);
@@ -16,21 +21,31 @@ public class ReportAbuse extends Dialog implements OnClickListener {
 	    setContentView(R.layout.report);
 	    setTitle("Reportar Abuso");
 	    
+	    mContext = context;
+	    
 	    okButton = (Button) findViewById(R.id.ok_report_button);
 	    okButton.setOnClickListener(this);
 	    
 	    cancellButton = (Button) findViewById(R.id.cancell_report_button);
 	    cancellButton.setOnClickListener(this);
-
+	    
+	    builder = new AlertDialog.Builder(mContext);
+    	builder.setMessage("Sua denúncia foi enviada e será avaliada. Obrigado!")
+    	       .setCancelable(false)
+    	       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+    	           public void onClick(DialogInterface dialog, int id) {
+    	        	   report.dismiss();
+    	           }
+    	       });
 	}
 	
 	@Override
-    public void onClick(View v) {  
-	    /*if (v == signupButton) {
-	    	Intent i = new Intent(getOwnerActivity().getApplicationContext(), Signup.class);
-	    	getOwnerActivity().startActivityForResult(i, ACTIVITY_CREATE);
-	    } else {*/
+    public void onClick(View v) {
+		
+	    if (v == okButton) {
+	    	builder.create().show();
+	    } else {
 	    	dismiss();
-	    //}
+	    }
 	 }
 }
